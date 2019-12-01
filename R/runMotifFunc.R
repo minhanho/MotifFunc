28abca59ab8f7c83e701f4f4f29aac2f9ab1973d
Minh An <minhan.ho@mail.utoronto.ca>
2019-12-01 00:37:34 -0500

    Barebones for shiny app

---
+++ R/runMotifFunc.R
@@ -0,0 +1,22 @@
+
+#' Launch the shiny app for package MotifFunc
+#'
+#' A function that launches the shiny app for this package.
+#' The code has been placed in \code{./inst/shiny-scripts}.
+#'
+#' @return No return value but open up a shiny page.
+#'
+#' @examples
+#' \dontrun{
+#' runMotifFunc()
+#' }
+#'
+#' @export
+#' @importFrom shiny runApp
+
+runMotifFunc <- function() {
+  appDir <- system.file("shiny-scripts",
+                        package = "MoifFunc")
+  shiny::runApp(appDir, display.mode = "normal")
+  return()
+}
---
+++ inst/shiny-scripts/app.R
@@ -0,0 +1,11 @@
+library(shiny)
+
+ui <- fluidPage(
+  #... code
+)
+
+server <- function(input, output) {
+  #... code
+}
+
+shinyApp(ui = ui, server = server)
