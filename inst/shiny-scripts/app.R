1cc23f55aca13eca8fba83c752e099e2bd5f3681
Minh An <minhan.ho@mail.utoronto.ca>
2019-12-01 22:40:01 -0500

    Scale plot

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -16,8 +16,14 @@ ui <- fluidPage(
                            h3("Sequence input (i.e. Composed of A,C,T, or/and G):")),
                  textOutput(outputId = "inputMessage"),
                  textOutput(outputId = "loadMessage"),
-                 tags$head(tags$style("#myplot{height:100vh !important;}"))
-                 ), mainPanel(plotOutput("plot", width = "100%")))
+                 tags$head(tags$script('$(document).on("shiny:connected", function(e) {
+                            Shiny.onInputChange("innerWidth", window.innerWidth);
+                            });
+                            $(window).resize(function(e) {
+                            Shiny.onInputChange("innerWidth", window.innerWidth);
+                            });
+                            '))
+                 ), mainPanel(plotOutput("plot")))
 
 )
 
@@ -39,7 +45,7 @@ server <- function(input, output) {
     }
     output$inputMessage <- renderText({message})
     output$loadMessage <- renderText({sprintf("Wordcloud visualization is loading...")})
-    output$plot <- renderPlot({MotifFunc::getFunctionWC(matchNames)}, width = 800, height = 600)
+    output$plot <- renderPlot({MotifFunc::getFunctionWC(matchNames)}, width = 800, height=reactive(ifelse(!is.null(input$innerWidth),input$innerWidth*3/5,0)))
     output$loadMessage <- renderText({sprintf("Wordcloud visualization is complete.")})
 
   })
